de78111880e8e42ec520292f7cb59f4d0ee7d124
Jonathan Davis <jonathan.davis@objectivecompany.com>
2011-11-18 04:10:12 -0700

    Added a config file to store Authorize.Net credentials and settings.

---
+++ config/authorizenet.php
@@ -0,0 +1,8 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+	'test_mode' => Kohana::$environment !== Kohana::PRODUCTION,
+	
+	'api_login'			=> 'your_login_id',
+	'transaction_key'	=> 'your_transaction_key',
+);
